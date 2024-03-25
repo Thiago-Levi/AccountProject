4b0498df381273b5bd115d02f1b9d128b42d498a
Levi's <110788808+Thiago-Levi@users.noreply.github.com>
2024-03-25 18:14:05 -0300

    Add files via upload

---
+++ BusinessAcount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcountProject.Entities
+{
+    internal class BusinessAcount : Account
+    {
+        public double LoanLimit { get; set; }
+         
+        public BusinessAcount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+           
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+
+        public override string ToString() 
+        {
+            return $"{LoanLimit}";
+        }
+      
+
+    }
+}
